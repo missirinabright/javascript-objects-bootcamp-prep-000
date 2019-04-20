347867e65737f19a71fe88dbebee005833f06e7f
Irina Bright <missirinabright@gmail.com>
2019-04-20 21:04:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -14,3 +14,4 @@ var playlist = ({artistName: 'Slowdive', songTitle: 'Allison'});
 //         to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
 //     })
 //   })
+// Create a function updatePlaylist that accepts three parameters: the playlist (an object), an artist name (a string), and a song title. The body of the function should add the song and artist as a key-value pair to the playlist object. The function should return the whole playlist.
